c84a16def2cca8106109f174b4b8e32041255a5a
carlosyanez <carlos.yanez.s@gmail.com>
2023-02-06 01:33:44 +1100

    WIP

---
+++ 4. Data/download.R
@@ -0,0 +1,29 @@
+library(here)
+library(fs)
+library(zip)
+library(piggyback)
+library(stringr)
+
+.rs.restartR()
+
+data_dir <- here("4. Data")
+
+#clean data dir
+files <- dir_ls(data_dir)
+files <- files[str_detect(files,"duckdb")]
+file_delete(files)
+
+#download files
+
+pb_download(dest=data_dir,repo="carlosyanez/MSc_DA_Project")
+
+files <- dir_ls(data_dir)
+files <- files[str_detect(files,"zip")]
+
+
+for (file in files){
+  unzip(file, junkpaths = TRUE,exdir = data_dir)
+}
+file_delete(files)
+
+rm(list=ls())
