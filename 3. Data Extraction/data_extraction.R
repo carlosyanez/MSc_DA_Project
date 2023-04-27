caf510f1e7bde10c22acb6be0cb35c8a47d9bf4b
carlosyanez <carlos.yanez.s@gmail.com>
2023-04-27 16:31:22 +1000

    reorg extraction

--- 3. Data Extraction/data_extraction.R
+++ 3. Data Extraction/data_extraction.R
@@ -6,6 +6,7 @@
 
 # load libraries 
 box::use(
+  readr[read_csv],
   dplyr[...],
   tidyr[...],
   stringr[...],
@@ -15,18 +16,21 @@ box::use(
   fs[...],
   DBI[...],
   dbplyr[...],
-  duckdb[...]
+  duckdb[...],
+  aussiemaps[geo_aggregate]
+  
   
 )
-
-# load custom functions
 
 # get folders
 run_folder <- here("3. Data Extraction")
 data_folder <- here("4. Data")
 
+# load custom functions
+source(path(run_folder,"functions.R"))
+
 # get names of aux files
-aux_files        <- dir_ls(run_folder,regexp = "edited\\.csv")
+aux_files        <- dir_ls(path(run_folder,"aux_files"),regexp = "edited\\.csv")
 names(aux_files) <- path_file(aux_files)
 
 # other variables
@@ -37,6 +41,9 @@ census_years <-c(2006,2011,2016,2021)
 year_equivalency <- tibble(election_years=election_years,census_years=census_years)
 codes <- c("CD_CODE_2006","SA1_7DIGITCODE_2011","SA1_7DIGITCODE_2016","SA1_CODE_2021")
 ceds  <- c("CED_NAME_2006","CED_NAME_2011","CED_NAME_2016","CED_NAME_2021")
+
+names(codes) <- census_years
+names(ceds)  <- census_years
 
 # Open database -----
 mydb <- dbConnect(duckdb(), path(data_folder,"consolidated_data.duckdb"),read_only=FALSE)
@@ -79,18 +86,12 @@ dbWriteTable(mydb, "primary_vote", primary_vote,overwrite=TRUE)
 var04 <- list_census_attributes(number="04") |>
          filter(str_detect(Attribute,"\\d{1,3}_\\d{1,3}_",TRUE)) |>
          filter(str_detect(Attribute,"\\d{1,3}-\\d{1,3}",TRUE))  |>
-         filter(str_detect(Attribute,"\\d{1,3}_persons$",TRUE))  |>
-         filter(str_detect(Attribute,"[Oo]ver",TRUE))  |>
-         filter(str_detect(Attribute,"[Tt]otal.*[Mm]ales",TRUE)) |>
+         filter(str_detect(Attribute,"[Mm]ale",TRUE))  |>
          pivot_longer(c(-Table,-Attribute), names_to="Year",values_to = "Value") |>
          mutate(Year=as.numeric(Year)) |>
          filter(!is.na(Value)) |>
          mutate(Age_at_census=str_extract(Attribute,"\\d{1,2}") |> as.numeric(),
-                YOB = Year-Age_at_census,
-                Gender=case_when(
-                str_detect(Attribute, "_[Mm]ales$") ~ "Males",
-                str_detect(Attribute, "_[Ff]emales$") ~ "Females",
-                TRUE ~"-"))
+                YOB = Year-Age_at_census)
 
 generations <- bind_rows(tibble(YOB=1883:1900,Group="Age - Lost Gen"),
                          tibble(YOB=1901:1927,Group="Age - Greatest Gen"),
@@ -112,5 +113,353 @@ levels <- attribute_tibble_to_list(var04,"Attribute","Group")
 
 #extract
 
-extract_census_ced("04",census_years,levels)
+age <- extract_census_ced("04",census_years,levels)
+dbWriteTable(mydb, "age", age,overwrite=TRUE)
 
+
+### Census Data - Education Level  -----
+
+census_tables <- list_census_tables() |>
+                   filter(Number %in% c("39","40","46","49")) |>
+                   filter(str_detect(`Table Name`,"Non-School")) |>             
+                   filter(str_detect(`Table Name`,"Level of Education by Age by Sex"))
+attributes    <- read_csv(aux_files["ed_levels_edited.csv"])
+levels        <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+ed_level      <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "ed_level", ed_level,overwrite=TRUE)
+
+
+### Census Data - Income           ------ 
+
+census_tables      <- list_census_tables() |>
+                        filter(Number %in% c("16","17")) |>
+                        filter(str_detect(`Table Name`,"School",TRUE)) |>
+                        filter(str_detect(`Table Name`,"Assistance",TRUE))
+attributes        <- read_csv(aux_files["income_levels_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+
+income            <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "income", income,overwrite=TRUE)
+
+### Census Data - Place of Birth           ------ 
+
+census_tables <- list_census_tables() |>
+                  filter(Number %in% c("09")) |>
+                  filter(str_detect(`Table Name`,"School",TRUE)) |>
+                  filter(str_detect(`Table Name`,"Assistance",TRUE))
+attributes        <- read_csv(aux_files["country_of_birth_levels_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+COB               <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "COB", COB,overwrite=TRUE)
+
+### Census Data - Religion           ------ 
+
+census_tables <- list_census_tables() |>
+                  filter(Number %in% c("13","14")) |>
+                  filter(str_detect(`Table Name`,"Religious"))
+attributes        <- read_csv(aux_files["religion_levels_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+religion          <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "religion", religion,overwrite=TRUE)
+
+### Census Data - Language           ------ 
+
+census_tables     <- list_census_tables() |>
+                      filter(Number %in% c("12","13")) |>
+                      filter(str_detect(`Table Name`,"Language")) |>
+                      filter(`Table Population`=="Persons")
+attributes        <- read_csv(aux_files["language_levels_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+language          <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "language", language,overwrite=TRUE)
+
+### Census Data - Citizenship           ------ 
+
+census_tables <- list_census_tables() |>
+                 filter(Number %in% c("01")) 
+attributes        <- read_csv(aux_files["citizenship_levels_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+citizenship        <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "citizenship", citizenship,overwrite=TRUE)
+
+### Census Data - Household Tenure       ------ 
+
+census_tables     <- list_census_tables() |>
+                      filter(str_detect(`Table Name`,"[Tt]enure")) 
+attributes        <- read_csv(aux_files["household_tenure_levels_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+household_tenure <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "household_tenure", household_ternure,overwrite=TRUE)
+
+### Census Data - Occupation       ------ 
+
+census_tables     <- list_census_tables() |>
+                      filter(str_detect(`Table Name`,"[In]ndustry")) |>
+                      filter(str_detect(`Table Name`,"[Oo]ccupation"))
+attributes        <- read_csv(aux_files["occupation_levels_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+occupation_tenure <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "occupation", occupation,overwrite=TRUE)
+
+### Census Data - Relationship Status      ------ 
+
+census_tables     <- list_census_tables() |>
+                      filter(str_detect(`Table Name`,"Relationship"))
+attributes        <- read_csv(aux_files["relationship_levels_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+relationship       <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "relationship", relationship,overwrite=TRUE)
+
+### Census Data - Prior Residence      ------ 
+
+census_tables     <- list_census_tables() |>
+                      filter(str_detect(`Table Name`,"5 Years"))
+attributes        <- read_csv(aux_files["prior_res_edited.csv"])
+levels            <- attribute_tibble_to_list(attributes)
+total_level       <- names(levels)[str_detect(names(levels),"Total")]
+
+relationship       <- extract_census_sa1(census_tables, census_years, levels,codes,ceds,total_level)
+dbWriteTable(mydb, "relationship", relationship,overwrite=TRUE)
+
+### Climate Data      ------ 
+
+library(sf)
+
+map_structure <- list_structure(2021) |>
+  select(GCCSA_CODE_2021,GCCSA_NAME_2021,
+         CED_CODE_2021,CED_NAME_2021,
+         Year)
+
+
+#electorates and and metro areas
+
+metro <-  map_structure |> 
+  distinct(GCCSA_CODE_2021,GCCSA_NAME_2021) |>
+  filter(str_detect(GCCSA_NAME_2021,"^Rest",TRUE)) |>
+  filter(str_detect(GCCSA_NAME_2021,"^No usual",TRUE)) |>
+  filter(str_detect(GCCSA_NAME_2021,"^Migratory",TRUE)) |>
+  filter(str_detect(GCCSA_NAME_2021,"^Other",TRUE))
+
+metro_electorates <- map_structure |>
+  count(Year,CED_CODE_2021, CED_NAME_2021,GCCSA_CODE_2021,GCCSA_NAME_2021) |>
+  group_by(Year,CED_CODE_2021, CED_NAME_2021) |>
+  filter(n==max(n)) |>
+  ungroup()  |>
+  select(-n) |>
+  filter(GCCSA_CODE_2021 %in% metro$GCCSA_CODE_2021) |>
+  filter(str_detect(CED_NAME_2021,"^Rest",TRUE)) |>
+  filter(str_detect(CED_NAME_2021,"^No usual",TRUE)) |>
+  filter(str_detect(CED_NAME_2021,"^Migratory",TRUE)) |>
+  filter(str_detect(CED_NAME_2021,"^Other",TRUE))
+
+non_metro <- map_structure |>
+  filter(!(CED_CODE_2021 %in% metro_electorates$CED_CODE_2021)) |>
+  filter(str_detect(CED_NAME_2021,"^Rest",TRUE)) |>
+  filter(str_detect(CED_NAME_2021,"^No usual",TRUE)) |>
+  filter(str_detect(CED_NAME_2021,"^Migratory",TRUE)) |>
+  filter(str_detect(CED_NAME_2021,"^Other",TRUE))      |>
+  select(CED_NAME_2021,CED_CODE_2021)
+
+
+
+metro_areas <-  bind_cols(metro,
+                          get_map(filters = list(GCCSA_CODE_2021 = metro$GCCSA_CODE_2021),
+                                  year=2021,
+                                  aggregation=c("GCCSA_NAME_2021"),
+                                  use_cache=TRUE) |>
+                            st_make_valid() |>
+                            st_centroid() |> 
+                            st_coordinates()) |>
+  rename("longitude"="X","latitude"="Y")
+
+
+non_metro_map <-get_map(filters = list(CED_CODE_2021 = non_metro$CED_CODE_2021),
+                        year=2021,
+                        aggregation=c("CED_NAME_2021"),
+                        use_cache=TRUE) 
+
+
+non_metro_areas <-  bind_cols(non_metro_map |> st_drop_geometry() |> select(CED_NAME_2021,CED_CODE_2021),
+                              non_metro_map |>st_make_valid() |>
+                                st_centroid() |> 
+                                st_coordinates() ) |> 
+  rename("longitude"="X","latitude"="Y")
+
+
+
+silodata_metro <- tibble()
+
+for(i in 1:nrow(metro_areas)){
+  
+  data_i  <- get_silodata(latitude = metro_areas[i,]$latitude,
+                          longitude = metro_areas[i,]$longitude,
+                          email = "carlos.yanez.s@gmail.com",
+                          START = "19700101",
+                          FINISH = "20220731") |>
+    mutate(GCCSA_NAME_2021 = metro_areas[i,]$GCCSA_NAME_2021,
+           GCCSA_CODE_2021 = metro_areas[i,]$GCCSA_CODE_2021
+    )
+  
+  silodata_metro <- bind_rows(silodata_metro,data_i)
+  
+  
+}
+
+
+silodata_non_metro <- tibble()
+
+for(i in 1:nrow(non_metro_areas)){
+  
+  data_i  <- get_silodata(latitude = non_metro_areas[i,]$latitude,
+                          longitude = non_metro_areas[i,]$longitude,
+                          email = "carlos.yanez.s@gmail.com",
+                          START = "19700101",
+                          FINISH = "20220731") |>
+    mutate(CED_NAME_2021 = non_metro_areas[i,]$CED_NAME_2021,
+           CED_CODE_2021 = non_metro_areas[i,]$CED_CODE_2021
+    )
+  
+  silodata_non_metro <- bind_rows(silodata_non_metro,data_i)
+  
+  
+}
+
+rm(data_i,i)
+
+dbWriteTable(mydb, "metro_electorates", metro_electorates,overwrite=TRUE)
+dbWriteTable(mydb, "non_metro_electorates", non_metro,overwrite=TRUE)
+
+
+dbWriteTable(mydb, "silodata_metro", silodata_metro,overwrite=TRUE)
+dbWriteTable(mydb, "silodata_non_metro", silodata_non_metro,overwrite=TRUE)
+
+
+
+### Proportions Tables -------
+
+for(year in census_years){
+  
+  if(year==2006){
+    original_geo <- "CD_CODE_2006"
+    new_geo      <- "CED_NAME_2006"
+  }
+  if(year %in% c(2011,2016)){
+    original_geo <- str_c("SA1_7DIGITCODE_",year)
+    new_geo      <- str_c("CED_NAME_",year)
+  }
+  if(year==2021){
+    original_geo <- str_c("SA1_CODE_",year)
+    new_geo      <- str_c("CED_NAME_",year)
+  }
+  
+  
+  correspondence_table <- list_structure(year=year) |>
+    select(any_of(c("id",original_geo,new_geo)))
+  
+  proportions_table <-   list_proportions(original_geo) |>
+    collect()  |>
+    select("id","geo_col","prop") |>
+    rename(!!original_geo := "geo_col")
+  
+  correspondence_table <- correspondence_table |>
+    left_join(proportions_table,by=c("id",original_geo)) |>
+    group_by(across(c(original_geo,new_geo))) |>
+    summarise(across(c("prop"), ~ sum(.x,na.rm=TRUE)),.groups = "drop") |>
+    filter(prop!=0) |>
+    mutate(Year=year)|>
+    mutate(across(any_of(new_geo), ~ case_when(
+      Year %in% c(2006,2011) & str_detect(.x,"Fraser") ~ "Fraser (I)",
+      Year %in% c(2019,2021) & str_detect(.x,"Fraser") ~ "Fraser (II)",
+      TRUE ~ .x
+    )))
+  
+  dbWriteTable(mydb, str_c("correspondence_",year), correspondence_table,overwrite=TRUE)
+  
+}
+
+
+
+### Fix CED Names ----
+## Census Data
+tables <- DBI::dbListTables(mydb)
+tables <- tables[str_detect(tables,"granular",TRUE)]
+tables <- tables[str_detect(tables,"year_equivalency",TRUE)]
+tables <- tables[str_detect(tables,"correspondence_",TRUE)]
+tables <- tables[str_detect(tables,"silodata",TRUE)]
+#tables <- tables[str_detect(tables,"electorates",TRUE)]
+#tables <- "language"
+#tables <- tables[str_detect(tables,"metro")]
+for(i in 1:length(tables)){
+  cols <- colnames(tbl(mydb,tables[i])) 
+  ced_col <- cols[str_detect(cols,"Unit|CED")]
+  
+  df <- tbl(mydb,tables[i]) |>
+    mutate(across(any_of(ced_col), ~ case_when(
+      Year %in% c(2006,2011) & str_detect(.x,"Fraser") ~ "Fraser (I)",
+      Year %in% c(2019,2021) & str_detect(.x,"Fraser") ~ "Fraser (II)",
+      TRUE ~ .x
+    ))) |>
+    collect()
+  
+  dbWriteTable(mydb, tables[i], df,overwrite=TRUE)
+  
+}
+# Primary Vote 
+primary_vote <- tbl(processed_db,"primary_vote") |>
+  left_join(tbl(processed_db,"year_equivalency"),by=c("Year"="election_years")) |>
+  collect() |>
+  mutate(DivisionNm=case_when(
+    census_years %in% c(2006,2011) & str_detect(DivisionNm,"Fraser") ~ "Fraser (I)",
+    census_years %in% c(2019,2021) & str_detect(DivisionNm,"Fraser") ~ "Fraser (II)",
+    TRUE ~ str_to_title(DivisionNm)
+  )) 
+
+dbWriteTable(mydb, "primary_vote",primary_vote,overwrite=TRUE)
+
+### Disconnect, clean, upload DB -----
+
+dbDisconnect(mydb, shutdown=TRUE)
+
+box::use(
+  piggyback[pb_new_release,pb_upload],
+  fs[file_delete],
+  zip[zip]
+)
+
+file_name      <- here("4. Data","consolidated_data.duckdb")
+repo           <- "carlosyanez/MSc_DA_Project"
+version       <- "data"
+
+#create new release
+tryCatch(pb_new_release(repo,version),
+         error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+
+#list and zip files
+zip_file <- "data_orig.zip"
+
+zip(zip_file,file_name,mode="cherry-pick")
+
+# upload catalogue items ---
+pb_upload(file=zip_file,repo,version)
+
+file_delete(zip_file)
+
+rm(list=ls())
--- 3. Data Extraction/functions.R
+++ 3. Data Extraction/functions.R
@@ -1,5 +1,6 @@
 ###### data extraction functions #####
 
+#' @export
 extract_census_ced <- function(table_number, census_years, levels) {
   
   age_and_sex <- tibble()
@@ -10,7 +11,7 @@ extract_census_ced <- function(table_number, census_years, levels) {
     } else{
       geo_structure_x <- "CED"
     }
-    
+    message(x)
     censusx <- get_census_summary(
       table_number = table_number,
       geo_structure = geo_structure_x,
@@ -34,4 +35,54 @@ extract_census_ced <- function(table_number, census_years, levels) {
     
   }
   return(age_and_sex)
+}
+
+#' @export
+extract_census_sa1 <- function(census_tables, census_years, levels,codes,ceds,total_value){
+  
+  df <- tibble()
+  
+  for(year in census_years){
+    
+    if(year=="2006"){
+      geo_structure_x <- "CD"
+    } else{
+      geo_structure_x <- "SA1"
+    }
+    
+    df_i <- get_census_summary(census_table = census_tables,
+                                         selected_years = year,
+                                         geo_structure=geo_structure_x,
+                                         attribute = levels) |>
+            filter(str_detect(Unit,"Shipping",TRUE))|>
+            filter(str_detect(Unit,"Usual",TRUE))   |>
+            filter(str_detect(Unit,"Not Applicable",TRUE)) 
+    
+    df_i <- df_i                                                  |>
+               select(-Unit)                                      |>
+               rename(!!codes[as.character(year)]:="Census_Code") |>
+               geo_aggregate(
+                              values_col="Value",
+                              original_geo=codes[as.character(year)],
+                              new_geo=ceds[as.character(year)],
+                              grouping_col = c("Attribute"),
+                              year=year)                      |>
+              rename("Unit"=unname(ceds[as.character(year)])) |>
+              filter(!is.na(Unit))                            |>
+              filter(str_detect(Unit,"Shipping",TRUE))        |>
+              filter(str_detect(Unit,"Usual",TRUE))           |>
+              filter(str_detect(Unit,"[Aa]pplicable",TRUE))   |>
+              calculate_percentage(key_col = "Attribute",
+                                      value_col = "Value",
+                                      key_value = total_value,
+                                      percentage_scale = 100)  |>
+              mutate(Year=year)
+    
+    df <- bind_rows(df,df_i)
+    
+  }
+  
+  return(df)
+  
+  
 }
