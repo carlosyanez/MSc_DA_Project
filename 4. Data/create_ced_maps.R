affe3787e48feff3c585aa814a070a5b3f61b12a
carlosyanez <carlos.yanez.s@gmail.com>
2023-05-14 11:13:50 +1000

    WIP + map generation

---
+++ 4. Data/create_ced_maps.R
@@ -0,0 +1,34 @@
+library(aussiemaps)
+library(sf)
+library(leaflet)
+library(htmlwidgets)
+library(glue)
+
+years <- as.numeric(colnames(aussiemaps::list_attributes() |> select(-attributes)))
+
+for(year in year){
+  map <- get_map(year=year,
+          aggregation = glue("CED_NAME_{year}"),
+          filter_table = list_structure(year=year),
+          new_crs = "EPSG:4326",
+          simplification_factor = 0.05,
+          use_cache = TRUE) |>
+          rename("DivisionNm"= glue("CED_NAME_{year}")) |>
+          select(Division,matches("STE|STATE"))
+  
+  
+  map_l <- map |>
+          leaflet()  |>
+          addTiles() |>
+          addPolygons(fillColor = "blue",
+                      fillOpacity = 0.4,
+                      label=~DivisionNm)
+  
+  saveWidget(map_l,
+             here("4. Data",glue("CED_{year}.html")),
+             libdir = here("4. Data","lib"),
+             selfcontained = FALSE)
+  
+  st_write(map,here("4. Data",glue("CED_{year}.gpkg")))
+  
+}
