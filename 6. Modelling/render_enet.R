3a6708774f1568b9c19ce4c0d38817a10ab2f7db
carlosyanez <carlos.yanez.s@gmail.com>
2023-05-31 21:07:58 +1000

    + delta regression

--- 6. Modelling/render_enet.R
+++ 6. Modelling/render_enet.R
@@ -3,12 +3,18 @@ library(here)
 library(glue)
 
 
-input_file  <- here("6. Modelling","regression_cluster.qmd")
+input_files  <- c(here("6. Modelling","regression_cluster.qmd"),
+                  here("6. Modelling","regression_cluster_delta.qmd"))
+
+tuning_grid <- seq(0,1,0.0002)
+
 
 setwd(here("6. Modelling"))
   
 for(c in c(0,1,2)){
-  quarto_render(input = input_file,
-                output_file = glue("regression_cluster_{c}.html"),
-                execute_params =list(cluster=c))
+  for(input_file in input_files){
+    quarto_render(input = input_file,
+                  output_file = glue("regression_cluster_{c}.html"),
+                  execute_params =list(cluster=c,a_grid))
+  }
 }             
