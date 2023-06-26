596517889b6f7e6a224cd89af69ecf8724003642
carlosyanez <carlos.yanez.s@gmail.com>
2023-06-26 11:51:53 +1000

    WIP

--- 8. Report/trackdown.R
+++ 8. Report/trackdown.R
@@ -8,7 +8,7 @@ gdrive_path <- "msc_project_proofreading"              # the folder on google dr
 filenames <- dir_ls(regexp = "Rmd")
   
   
-filename <- filenames[6]
+filename <- filenames[9]
   
 #upload for first time
 trackdown::upload_file(filename, gpath=gdrive_path,hide_code=TRUE) 
